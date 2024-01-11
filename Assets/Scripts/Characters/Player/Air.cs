7ae04d0010c35a95db15b3418da5dfeedab87ef4
nhutuyen <nhutuyendev@gmail.com>
2024-01-12 01:58:17 +0700

    Fix Air script

--- Assets/Scripts/Characters/Player/Air.cs
+++ Assets/Scripts/Characters/Player/Air.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 
 public class Air : BaseAir
 {
-    public Transform firePos;
-    public GameObject bulletPrefab;
+    public Transform firePos_1;
+    public Transform firePos_2;
+    public Transform firePos_3;
+    public Transform firePos_4;
+    public Transform firePos_5;
+
+    public GameObject Sheild;
+
+
+    [SerializeField] GameObject bulletPrefab;
+
     public float timeBtwFire = 0.2f;
 
     float _timeBtwFire;
+
 
     protected override void FireBullet()
     {
@@ -19,16 +29,26 @@ public class Air : BaseAir
         {
             _timeBtwFire = timeBtwFire;
             // GameObject bulletClone = Instantiate(bulletPrefab, firePos.position, Quaternion.identity);
-            SimplePool.Spawn(bulletPrefab, firePos.position, Quaternion.identity);
+            SimplePool.Spawn(bulletPrefab.gameObject, firePos_1.position, Quaternion.identity);
+
+            if (firePos_2.gameObject.activeInHierarchy)
+                SimplePool.Spawn(bulletPrefab.gameObject, firePos_2.position, Quaternion.identity);
+            if (firePos_3.gameObject.activeInHierarchy)
+                SimplePool.Spawn(bulletPrefab.gameObject, firePos_3.position, Quaternion.identity);
+            if (firePos_4.gameObject.activeInHierarchy)
+                SimplePool.Spawn(bulletPrefab.gameObject, firePos_4.position, Quaternion.identity);
+            if (firePos_5.gameObject.activeInHierarchy)
+                SimplePool.Spawn(bulletPrefab.gameObject, firePos_5.position, Quaternion.identity);
+            AudioController.Instance.PlaySound(AudioController.Instance.shoot);
 
 
             /*Rigidbody2D rb = bulletClone.GetComponent<Rigidbody2D>();
             rb.AddForce(transform.up * bulletForce, ForceMode2D.Impulse);*/
         }
-        
+
     }
 
 
 
-    
+
 }
